43c2131b58d05a447867188e5dc1b2bb11e1b061
javad <javad936807@gmail.com>
2022-06-04 18:44:41 +0430

    add for test some code

---
+++ test.py
@@ -0,0 +1,13 @@
+# importing the module
+import json
+import math
+import numpy as np
+ 
+# Opening JSON file
+# with open('dataset.json') as json_file:
+#     data = json.load(json_file)
+
+ 
+#     # Print the data of dictionary
+#     print("data set 1::", data["آدرس فروشگاه کجاست؟"])
+print(1+np.log(3/2))
