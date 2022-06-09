5ddf14e78661055aa27598a15f1a483a97f82ca4
javad <javad936807@gmail.com>
2022-06-10 02:05:59 +0430

    almost done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import enum
 import json
 import re
+import sys
 from db import db
 from Response import Response
 
@@ -22,7 +23,7 @@ def listener(messages):
     for m in messages:
         chatid = m.chat.id #give user id for resend message 
 
-        if m.content_type == 'text' and m.text!="/help" and m.text !="/start" and m.text !="/items": #check format message has text and doesn't has /help or /start 
+        if m.content_type == 'text' and m.text!="/help" and m.text !="/start" and m.text !="/items" and m.text!="/allOrder": #check format message has text and doesn't has /help or /start 
             # s =  Similarities([])
             with open("question.json", "r") as file:
                 dataQ = json.load(file)
@@ -38,32 +39,51 @@ def listener(messages):
             print(text)
             
             logging.info('ci='+str(chatid)+"-un ="+username+"-fn:"+first_name+"-ln:"+last_name+"-t="+text)
-            response, group, err = r.getResponse(str(text.strip()))
+            response, group, nearQ, err = r.getResponse(str(text.strip()))
             print("g="+group)
+            # bot.send_message(chatid, t.getTempOrder(chatid))
             
-            if err ==1 and t.getFlag(chatid)==0:
-                      bot.send_message(chatid, "توان فهم ورودی شما را ندارم\nلطفا واضح تر بیان کنید")
+            if text=="فاکتور":
+                      tempOrder= t.getTempOrder(chatid)
                       
-            
-                      
-            #normal status
-            if group != "Shop" and err == 0 and t.getFlag(chatid) == 0:
-                      bot.send_message(chatid, response)
-            
-            if group == "Shop" and err == 0 and t.getFlag(chatid) == 0 :
-                      t.changeFlagUser(chatid,1)
-                      bot.send_message(chatid, response)
-                      
-            elif   t.getFlag(chatid) == 1:
-                      isNumber = re.search("\d+", text)
-                      if isNumber==None:
-                                bot.send_message(chatid, " لطفا مقدار وارد کنید ")
-                      else :
-                        t.changeFlagUser(chatid,0)
-                        number = isNumber
-                        bot.send_message(chatid, number.group())
-                        
-            print("fl="+str(t.getFlag(chatid)))
+                      if tempOrder != None:
+                        bot.send_message(chatid, tempOrder)
+                      else:
+                                bot.send_message(chatid, "سفارشی ندارید" )
+                                
+            elif text =="*بله":
+                      t.acceptAllOrder(chatid)
+                      bot.send_message(chatid, "عملیات با موفقیت انجام شد" )
+            elif text =="*خیر":
+                      bot.send_message(chatid, "ادامه بدهید به سفارش" )
+            else :
+                if err ==1 and t.getFlag(chatid)==0:
+                          bot.send_message(chatid, "توان فهم ورودی شما را ندارم\nلطفا واضح تر بیان کنید")
+
+
+                #normal status
+                if group != "Shop" and err == 0 and t.getFlag(chatid) == 0:
+                          bot.send_message(chatid, response)
+
+                if group == "Shop" and err == 0 and t.getFlag(chatid) == 0 :
+                          t.changeFlagUser(chatid,1)
+                          t.setTempItem(chatid ,nearQ)
+                          bot.send_message(chatid, response)
+
+                elif   t.getFlag(chatid) == 1:
+                          isNumber = re.search("\d+", text)
+
+                          if isNumber==None:
+                                    bot.send_message(chatid, " لطفا مقدار را با واحد کیلو وارد کنید ")
+                          else :
+                            t.changeFlagUser(chatid,0)
+                            number = isNumber   
+                            tempIndex = t.getTempIndexFromUser(str(chatid))
+                            t.setOrder(chatid, tempIndex, number.group(), 0)
+                            
+                            bot.send_message(chatid, "بسیار خب سفارشتان ثبت شد ، برای نمایش لیست خرید فاکتور را تایپ کنید")
+
+                # print("fl="+str(t.getFlag(chatid)))
                         
                         
                       
@@ -83,9 +103,16 @@ def help(message):
 @bot.message_handler(commands =["items"])
 def items(message):
       bot.reply_to(message,"سیب - انار -گلابی- بستنی- شیر - موز")
-
       
-# bot.polling()
+@bot.message_handler(commands =["allOrder"])
+def allOrder(message):
+      d = db()
+      orders = d.allOrder(message.chat.id )
+      if orders !=None:
+        bot.reply_to(message, orders)
+      else :
+                bot.reply_to(message, "سفارشی ندارید")
+
 
 
 bot.set_update_listener(listener) #register listener
